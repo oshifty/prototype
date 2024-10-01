5cbad42301a92a98f6b4b3eacedbbf046ed1f936
Hannes Rüger <hannes@rueger.events>
2024-10-01 13:48:31 +0200

    feat (node desk): implement basic connection flow

---
+++ desks/node/src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { invalidateAll } from "$app/navigation";
+    import { io } from "$lib/socket";
+    import { onMount } from "svelte";
+
+    onMount(() => {
+        io.on("refresh", () => {
+            console.log("refresh");
+            invalidateAll();
+        });
+        setTimeout(() => {
+            invalidateAll();
+        }, 1000);
+    });
+</script>
+
+<slot />
