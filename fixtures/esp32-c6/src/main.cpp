436aeeffcd9e537ebd97ab29e547d1f2a1cc93e1
Lukas Runge <login@lukasrunge.de>
2024-10-01 15:54:02 +0200

    ✨ add tcp socket server

--- fixtures/esp32-c6/src/main.cpp
+++ fixtures/esp32-c6/src/main.cpp
@@ -6,6 +6,7 @@
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
 */
+#include <lwip/netdb.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -19,11 +20,19 @@
 #include "freertos/event_groups.h"
 #include "freertos/task.h"
 #include "led_strip.h"
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
 #include "mdns.h"
 #include "nvs_flash.h"
 #include "sdkconfig.h"
 
-static const char* TAG = "example";
+#define PORT 4284
+#define KEEPALIVE_IDLE 7200
+#define KEEPALIVE_INTERVAL 75
+#define KEEPALIVE_COUNT 9
+
+static const char *TAG = "example";
 
 /* The examples use WiFi configuration that you can set via project configuration menu
 
@@ -74,8 +83,8 @@ static EventGroupHandle_t s_wifi_event_group;
 
 static int s_retry_num = 0;
 
-static void event_handler(void* arg, esp_event_base_t event_base,
-                          int32_t event_id, void* event_data) {
+static void event_handler(void *arg, esp_event_base_t event_base,
+                          int32_t event_id, void *event_data) {
     if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
         esp_wifi_connect();
     } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
@@ -88,7 +97,7 @@ static void event_handler(void* arg, esp_event_base_t event_base,
         }
         ESP_LOGI(TAG, "connect to the AP fail");
     } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-        ip_event_got_ip_t* event = (ip_event_got_ip_t*)event_data;
+        ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
         ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
         s_retry_num = 0;
         xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
@@ -177,7 +186,7 @@ void start_mdns_service() {
 
 void add_mdns_services() {
     // add our services
-    mdns_service_add(NULL, "_scf", "_tcp", 4284, NULL, 0);
+    mdns_service_add(NULL, "_scf", "_tcp", PORT, NULL, 0);
 
     // NOTE: services must be added before their properties can be set
     // use custom instance for the web server
@@ -191,6 +200,110 @@ void add_mdns_services() {
 
     // set txt data for service (will free and replace current data)
     mdns_service_txt_set("_scf", "_tcp", serviceTxtData, 4);
+}
+
+static void tcp_server_task(void *pvParameters) {
+    char addr_str[128];
+    int addr_family = (int)pvParameters;
+    int ip_protocol = 0;
+    int keepAlive = 1;
+    int keepIdle = KEEPALIVE_IDLE;
+    int keepInterval = KEEPALIVE_INTERVAL;
+    int keepCount = KEEPALIVE_COUNT;
+    struct sockaddr_storage dest_addr;
+
+    if (addr_family == AF_INET) {
+        struct sockaddr_in *dest_addr_ip4 = (struct sockaddr_in *)&dest_addr;
+        dest_addr_ip4->sin_addr.s_addr = htonl(INADDR_ANY);
+        dest_addr_ip4->sin_family = AF_INET;
+        dest_addr_ip4->sin_port = htons(PORT);
+        ip_protocol = IPPROTO_IP;
+    }
+
+    int listen_sock = socket(addr_family, SOCK_STREAM, ip_protocol);
+    if (listen_sock < 0) {
+        ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
+        vTaskDelete(NULL);
+        return;
+    }
+    int opt = 1;
+    setsockopt(listen_sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    // Note that by default IPV6 binds to both protocols, it is must be disabled
+    // if both protocols used at the same time (used in CI)
+    setsockopt(listen_sock, IPPROTO_IPV6, IPV6_V6ONLY, &opt, sizeof(opt));
+
+    ESP_LOGI(TAG, "Socket created");
+
+    int err = bind(listen_sock, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
+    if (err != 0) {
+        ESP_LOGE(TAG, "Socket unable to bind: errno %d", errno);
+        ESP_LOGE(TAG, "IPPROTO: %d", addr_family);
+        goto CLEAN_UP;
+    }
+    ESP_LOGI(TAG, "Socket bound, port %d", PORT);
+
+    err = listen(listen_sock, 1);
+    if (err != 0) {
+        ESP_LOGE(TAG, "Error occurred during listen: errno %d", errno);
+        goto CLEAN_UP;
+    }
+
+    while (1) {
+        ESP_LOGI(TAG, "Socket listening");
+
+        struct sockaddr_storage source_addr;  // Large enough for both IPv4 or IPv6
+        socklen_t addr_len = sizeof(source_addr);
+        int sock = accept(listen_sock, (struct sockaddr *)&source_addr, &addr_len);
+        if (sock < 0) {
+            ESP_LOGE(TAG, "Unable to accept connection: errno %d", errno);
+            break;
+        }
+
+        // Set tcp keepalive option
+        setsockopt(sock, SOL_SOCKET, SO_KEEPALIVE, &keepAlive, sizeof(int));
+        setsockopt(sock, IPPROTO_TCP, TCP_KEEPIDLE, &keepIdle, sizeof(int));
+        setsockopt(sock, IPPROTO_TCP, TCP_KEEPINTVL, &keepInterval, sizeof(int));
+        setsockopt(sock, IPPROTO_TCP, TCP_KEEPCNT, &keepCount, sizeof(int));
+        // Convert ip address to string
+        if (source_addr.ss_family == PF_INET) {
+            inet_ntoa_r(((struct sockaddr_in *)&source_addr)->sin_addr, addr_str, sizeof(addr_str) - 1);
+        }
+
+        ESP_LOGI(TAG, "Socket accepted ip address: %s", addr_str);
+
+        // receive protobuf message length
+        char rx_buffer_len[4];
+        int success = recv(sock, rx_buffer_len, sizeof(rx_buffer_len), 0);
+
+        if (success < 0) {
+            ESP_LOGE(TAG, "Error occurred during receiving: errno %d", errno);
+        } else if (success == 0) {
+            ESP_LOGW(TAG, "Connection closed");
+        } else {
+            int len = 0;
+            len = (rx_buffer_len[3] << 24) | (rx_buffer_len[2] << 16) | (rx_buffer_len[1] << 8) | rx_buffer_len[0];
+
+            if (len > 0) {
+                char *rx_buffer_msg = (char *)malloc(len);
+                int success = recv(sock, rx_buffer_msg, sizeof(rx_buffer_len), 0);
+                if (success < 0) {
+                    ESP_LOGE(TAG, "Error occurred during receiving: errno %d", errno);
+                } else if (success == 0) {
+                    ESP_LOGW(TAG, "Connection closed");
+                } else {
+                    ESP_LOGI(TAG, "Received %d bytes: %s", len, rx_buffer_msg);
+                }
+            }
+        }
+
+        shutdown(sock, 0);
+        close(sock);
+    }
+
+CLEAN_UP:
+    close(listen_sock);
+    vTaskDelete(NULL);
 }
 
 /* Use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
@@ -274,12 +387,17 @@ extern "C" void app_main(void) {
         ret = nvs_flash_init();
     }
     ESP_ERROR_CHECK(ret);
+    ESP_ERROR_CHECK(esp_netif_init());
 
     ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
     wifi_init_sta();
 
+    // ESP_ERROR_CHECK(esp_event_loop_create_default());
+
     start_mdns_service();
     add_mdns_services();
+
+    xTaskCreate(tcp_server_task, "tcp_server", 4096, (void *)AF_INET, 5, NULL);
 
     /* Configure the peripheral according to the LED type */
     configure_led();
